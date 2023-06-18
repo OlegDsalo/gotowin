c6b0c4ceda7c979f66d259d85e3f7bc9dac5c359
OlegDsalo <oleg.d.salo@gmail.com>
2023-06-18 21:54:35 +0300

    master:end adaptive css, update readmy file, change to module file

--- src/components/BlockChain/BlockChain.js
+++ src/components/BlockChain/BlockChain.js
@@ -2,7 +2,7 @@ import React from 'react';
 import binanceCoin from "../../assets/coins/BinanceCoin2.png";
 import Input from "../../components-ui/Input/Input";
 import Button from "../../components-ui/Button/Button";
-import './BlockChain.scss'
+import classes from './BlockChain.module.scss'
 import binance from '../../assets/coins/binanceUser.png'
 import bitcoin from '../../assets/coins/bitcoinUser.png'
 import lite from '../../assets/coins/liteUser.png'
@@ -12,27 +12,27 @@ import cardano from '../../assets/coins/cardanoUser.png'
 
 const BlockChain = () => {
     return (
-        <div className="purchase">
-            <img src={binance} alt="" className='purchase__coin purchase__coin_binance'/>
-            <img src={bitcoin} alt="" className='purchase__coin purchase__coin_bitcoin'/>
-            <img src={lite} alt="" className='purchase__coin purchase__coin_lite'/>
-            <img src={stellar} alt="" className='purchase__coin purchase__coin_stellar'/>
-            <img src={ethereum} alt="" className='purchase__coin purchase__coin_ethereum'/>
-            <img src={cardano} alt="" className='purchase__coin purchase__coin_cardano'/>
-            <div className="title">Buy & Win</div>
-            <div className="purchase-subtitle">Exchange blockchain in a few clicks</div>
-            <div className="purchase-calculator-bg">
-                <div className="purchase-calculator">
-                    <div className="purchase-title">Blockchain</div>
-                    <div className="purchase-button"><img src={binanceCoin} alt=""/>BSC</div>
-                    <hr className='purchase-line'/>
-                    <div className='purchase-label'>Chose price</div>
-                    <div className="purchase_inputs">
-                        <Input className='purchase_input'/>
-                        <Input className='purchase_input'/>
+        <div className={classes.purchase}>
+            <img src={binance} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_binance}`}/>
+            <img src={bitcoin} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_bitcoin}`}/>
+            <img src={lite} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_lite}`}/>
+            <img src={stellar} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_stellar}`}/>
+            <img src={ethereum} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_ethereum}`}/>
+            <img src={cardano} alt="" className={`${classes.purchase__coin} ${classes.purchase__coin_cardano}`}/>
+            <div className={classes.title}>Buy & Win</div>
+            <div className={classes.purchase__subtitle}>Exchange blockchain in a few clicks</div>
+            <div className={classes.purchase__calculator_bg}>
+                <div className={classes.purchase__calculator}>
+                    <div className={classes.purchase__title}>Blockchain</div>
+                    <div className={classes.purchase__button_icon}><img src={binanceCoin} alt=""/>BSC</div>
+                    <hr className={classes.purchase__line}/>
+                    <div className={classes.purchase__label}>Chose price</div>
+                    <div className={classes.purchase__inputs}>
+                        <Input className={classes.purchase__input}/>
+                        <Input className={classes.purchase__input}/>
                     </div>
-                    <hr className='purchase-line'/>
-                    <Button className='purchase_button'>Buy</Button>
+                    <hr className={classes.purchase__line}/>
+                    <Button className={classes.purchase__button}>Buy</Button>
                 </div>
             </div>
         </div>
